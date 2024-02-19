cba25c2c2fae095b31152f7bc71c78db31e76114
Nishan Holla <hollanishan@gmail.com>
2024-02-19 17:11:00 +0530

    tokens in evn file

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var axios = require("axios");
+require('dotenv').config()
 
-const api_token = 'e0f391e95c6268fd7224a12e0018790d694f746568715628';
-const api_key = '471e6c0621aeca7137cf011fdcc9900c5c1fa0322087154a';
 const subdomain = 'api.exotel.com';
 const sendPhone = '8951621998';
 
@@ -23,9 +22,10 @@ function sendOtp(api,dataString){
 app.post('/sendOtp',(req,res)=>{
     let sid = req.body.Sid;
     let phn = req.body.phn;
-    let api = `https://${api_token}:${api_key}${subdomain}/v1/Accounts/${sid}/Sms/send`;    
+    let api = `https://${process.env.APITOKEN}:${process.env.APIKEY}${subdomain}/v1/Accounts/${sid}/Sms/send`;    
     let dataString = `From=${sendPhone}&To=${phn}&Body=Hello World!&ShortenUrl=true&ShortenUrlParams[Tracking]=true&ShortenUrlParams[ClickTrackingCallbackUrl]=https://www.google.co.in/`;
     // sendOtp(api,dataString);
+    console.log(process.env.PORT);
     res.json({"message":[api,dataString]})
 })
 
